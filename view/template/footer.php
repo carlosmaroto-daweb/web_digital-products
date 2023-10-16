1aac96d0d9c762d3fd7d0ffa4832a9338e840918
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-16 14:56:23 +0200

    v1.0

--- view/template/footer.php
+++ view/template/footer.php
@@ -44,29 +44,7 @@
         
         
         <!-- JS -->
-        <script src="view/assets/js/jquery.min.js"></script>
-        <script src="view/assets/js/jquery.easing.1.3.js"></script>
-        <script src="view/assets/js/bootstrap.bundle.min.js"></script>
-        <script src="view/assets/js/SmoothScroll.js"></script>
-        <script src="view/assets/js/jquery.scrollTo.min.js"></script>
-        <script src="view/assets/js/jquery.localScroll.min.js"></script>
-        <script src="view/assets/js/jquery.viewport.mini.js"></script>
-        <script src="view/assets/js/jquery.parallax-1.1.3.js"></script>
-        <script src="view/assets/js/jquery.fitvids.js"></script>
-        <script src="view/assets/js/owl.carousel.min.js"></script>
-        <script src="view/assets/js/isotope.pkgd.min.js"></script>
-        <script src="view/assets/js/imagesloaded.pkgd.min.js"></script>
-        <script src="view/assets/js/jquery.magnific-popup.min.js"></script>
-        <script src="view/assets/js/masonry.pkgd.min.js"></script>
-        <script src="view/assets/js/jquery.lazyload.min.js"></script>
-        <script src="view/assets/js/wow.min.js"></script>
-        <script src="view/assets/js/morphext.js"></script>
-        <script src="view/assets/js/typed.min.js"></script>
-        <script src="view/assets/js/all.js"></script>
-        <script src="view/assets/js/contact-form.js"></script>
-        <script src="view/assets/js/jquery.ajaxchimp.min.js"></script>
-        <script src="view/assets/js/objectFitPolyfill.min.js"></script>
-        <script src="view/assets/js/splitting.min.js"></script>
+        <?php require_once "view/template/script.php"?>
         
     </body>
 </html>
