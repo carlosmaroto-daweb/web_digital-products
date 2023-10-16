ea398728de1ef1b32e2f23dedb02a7dde4bd84ea
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-16 13:34:23 +0200

    v1.0

---
+++ view/template/footer.php
@@ -0,0 +1,72 @@
+            <!-- Footer -->
+            <footer class="page-section bg-dark-lighter light-content footer pb-100 pb-sm-50">
+                <div class="container">
+                    
+                    <!-- Social Links -->
+                    <div class="footer-social-links mb-90 mb-xs-40">
+                        <a href="#" title="Facebook" target="_blank"><i class="fab fa-facebook-f"></i> <span class="sr-only">Facebook profile</span></a>
+                        <a href="#" title="Twitter" target="_blank"><i class="fab fa-twitter"></i> <span class="sr-only">Twitter profile</span></a>
+                        <a href="#" title="Twitter" target="_blank"><i class="fab fa-vimeo-v"></i> <span class="sr-only">Vimeo profile</span></a>
+                        <a href="#" title="Behance" target="_blank"><i class="fab fa-behance"></i> <span class="sr-only">Behance profile</span></a>
+                        <a href="#" title="Pinterest" target="_blank"><i class="fab fa-pinterest"></i> <span class="sr-only">Pinterest profile</span></a>
+                    </div>
+                    <!-- End Social Links -->
+                    
+                    <!-- Footer Text -->
+                    <div class="footer-text">
+                        
+                        <!-- Copyright -->
+                        <div class="footer-copy">
+                            <a href="index.html">© Rhythm 2023</a>.
+                        </div>
+                        <!-- End Copyright -->
+                        
+                        <div class="footer-made">
+                            Made with love for great people.
+                        </div>
+                        
+                    </div>
+                    <!-- End Footer Text --> 
+                    
+                 </div>
+                 
+                 <!-- Top Link -->
+                 <div class="local-scroll">
+                     <a href="#top" class="link-to-top"><i class="link-to-top-icon"></i><span class="sr-only">Scroll to top</span></a>
+                 </div>
+                 <!-- End Top Link -->
+                 
+            </footer>
+            <!-- End Footer -->
+        
+        </div>
+        <!-- End Page Wrap -->
+        
+        
+        <!-- JS -->
+        <script src="view/assets/js/jquery.min.js"></script>
+        <script src="view/assets/js/jquery.easing.1.3.js"></script>
+        <script src="view/assets/js/bootstrap.bundle.min.js"></script>
+        <script src="view/assets/js/SmoothScroll.js"></script>
+        <script src="view/assets/js/jquery.scrollTo.min.js"></script>
+        <script src="view/assets/js/jquery.localScroll.min.js"></script>
+        <script src="view/assets/js/jquery.viewport.mini.js"></script>
+        <script src="view/assets/js/jquery.parallax-1.1.3.js"></script>
+        <script src="view/assets/js/jquery.fitvids.js"></script>
+        <script src="view/assets/js/owl.carousel.min.js"></script>
+        <script src="view/assets/js/isotope.pkgd.min.js"></script>
+        <script src="view/assets/js/imagesloaded.pkgd.min.js"></script>
+        <script src="view/assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="view/assets/js/masonry.pkgd.min.js"></script>
+        <script src="view/assets/js/jquery.lazyload.min.js"></script>
+        <script src="view/assets/js/wow.min.js"></script>
+        <script src="view/assets/js/morphext.js"></script>
+        <script src="view/assets/js/typed.min.js"></script>
+        <script src="view/assets/js/all.js"></script>
+        <script src="view/assets/js/contact-form.js"></script>
+        <script src="view/assets/js/jquery.ajaxchimp.min.js"></script>
+        <script src="view/assets/js/objectFitPolyfill.min.js"></script>
+        <script src="view/assets/js/splitting.min.js"></script>
+        
+    </body>
+</html>
---
+++ view/template/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!-- Change the value of lang="en" attribute if your website's language is not English.
+You can find the code of your language here - https://www.w3schools.com/tags/ref_language_codes.asp -->
+<html lang="en">
+    <head>
+        <title>Rhythm &mdash; One & Multi Page Creative Template</title>
+        <meta name="description" content="Rhythm &mdash; One & Multi Page Creative Template">  
+        <meta charset="utf-8">
+        <meta name="author" content="https://themeforest.net/user/bestlooker/portfolio">
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <!-- Favicons -->
+        <link rel="shortcut icon" href="view/assets/img/favicon.png">
+
+        <!-- CSS -->
+        <?php require_once "view/template/style.php"?>
+        
+    </head>
+    <body class="appear-animate">
+        
+        <!-- Page Loader -->        
+        <div class="page-loader dark">
+            <div class="loader">Loading...</div>
+        </div>
+        <!-- End Page Loader -->
+
+        <!-- Skip to Content -->
+        <a href="#main" class="btn skip-to-content">Skip to Content</a>
+        <!-- End Skip to Content -->
+        
+        <!-- Page Wrap -->
+        <div class="page bg-dark light-content" id="top">
+            
+            <!-- Navigation panel -->
+            <nav class="main-nav dark transparent stick-fixed wow-menubar">
+                <div class="full-wrapper relative clearfix">
+                    
+                    <!-- Logo ( * your text or image into link tag *) -->
+                    <div class="nav-logo-wrap local-scroll">
+                        <a href="index.html" class="logo">
+                            <img src="view/assets/img/logo-white.png" alt="Company logo" width="188" height="37" />
+                        </a>
+                    </div>
+                    
+                    <!-- Mobile Menu Button -->
+                    <div class="mobile-nav" role="button" tabindex="0">
+                        <i class="fa fa-bars"></i>
+                        <span class="sr-only">Menu</span>
+                    </div>
+                    
+                    <!-- Main Menu -->
+                    <div class="inner-nav desktop-nav">
+                        <ul class="clearlist scroll-nav local-scroll">
+                            <li class="active"><a href="#home">Home</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#services">Services</a></li>
+                            <li><a href="#portfolio">Portfolio</a></li>
+                            <li><a href="#blog">Blog</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                            
+                            <li><a href="intro.html">All Demos</a></li>
+                        </ul>
+                    </div>
+                    <!-- End Main Menu -->
+                    
+                </div>
+            </nav>
+            <!-- End Navigation panel -->
