32fbeafc23269788fd12507c7f0452cab2678071
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-17 12:17:19 +0200

    v1.0

---
+++ view/cookies.php
@@ -0,0 +1,124 @@
+            <main id="main">
+                <!-- Home Section -->
+                <section class="page-section bg-dark-alfa-50 bg-scroll" data-background="images/full-width-images/section-bg-19.jpg" id="home">
+                    <div class="container relative">
+                    
+                        <div class="row">
+                            
+                            <div class="col-md-8">
+                                <div class="wow fadeInUpShort" data-wow-delay=".1s">
+                                    <h1 class="hs-line-7 mb-20 mb-xs-10">FAQ</h1>
+                                </div>
+                                <div class="wow fadeInUpShort" data-wow-delay=".2s">
+                                    <p class="hs-line-6 opacity-075 mb-20 mb-xs-0">
+                                        Frequently Asked Questions
+                                    </p>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-4 mt-30 wow fadeInUpShort" data-wow-delay=".1s">
+                                <div class="mod-breadcrumbs text-end">
+                                    <a href="#">Home</a>&nbsp;<span class="mod-breadcrumbs-slash">•</span>&nbsp;<a href="#">Pages</a>&nbsp;<span class="mod-breadcrumbs-slash">•</span>&nbsp;<span>FAQ</span>
+                                </div>                                
+                            </div>
+                            
+                        </div>
+                    
+                    </div>
+                </section>
+                <!-- End Home Section -->
+                
+                
+                <!-- Section -->
+                <section class="page-section bg-dark light-content">
+                    <div class="container relative">
+                        
+                        <div class="row mb-80 mb-xs-40 wow fadeInUpShort">
+                            <div class="col-md-8 offset-md-2">
+                                <div class="section-text align-center">
+                                    
+                                    <!-- Search -->
+                                    <form class="form mb-20">
+                                        <div class="search-wrap">                                            
+                                            <input type="text" class="form-control search-field round" placeholder="Search...">
+                                            <button class="search-button animate" type="submit" title="Start Search">
+                                                <i class="fa fa-search"></i>
+                                            </button>
+                                        </div>
+                                    </form>
+                                    <!-- End Search -->
+                                    
+                                     Cras mattis est eget lacus dignissim, non consectetur velit pellentesque. Ut sit amet placerat eros, id&nbsp;gravida arcu. Sed felis purus, tempor ac vestibulum nec, consectetur at purus. 
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="row section-text wow fadeInUpShort">
+                            <div class="col-md-8 offset-md-2">
+                                
+                                <hr class="mb-30 white"/>
+                                
+                                <h4 class="font-alt">What do you mean by an End Product?</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula porttitor rutrum. Utore sodales rutrum. Ut eget tristique nibh. Cras mattis est eget lacus dignissim, non consectetur veliterme pellentesque. Ut sit amet placerat eros, id gravida arcu. Sed felis purus, tempor ac vestibulum nec, consectetur at purus. 
+                                </p>
+                                
+                                <hr class="mb-30 white"/>
+                                
+                                <h4 class="font-alt">I have bought a product that has files missing, what do I do?</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula porttitor rutrum. Utore sodales rutrum. Ut eget tristique nibh. Cras mattis est eget lacus dignissim, non consectetur veliterme pellentesque. Ut sit amet placerat eros, id gravida arcu. Sed felis purus, tempor ac vestibulum nec, consectetur at purus. 
+                                </p>
+                                
+                                <hr class="mb-30 white"/>
+                                
+                                <h4 class="font-alt">Do I need to buy a licence for each site?</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula porttitor rutrum. Utore sodales rutrum. Ut eget tristique nibh. Cras mattis est eget lacus dignissim, non consectetur veliterme pellentesque. Ut sit amet placerat eros, id gravida arcu. Sed felis purus, tempor ac vestibulum nec, consectetur at purus. 
+                                </p>
+                                
+                                <hr class="mb-30 white"/>
+                                
+                                <h4 class="font-alt">Is my license transferable?</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula porttitor rutrum. Utore sodales rutrum. Ut eget tristique nibh. Cras mattis est eget lacus dignissim, non consectetur veliterme pellentesque. Ut sit amet placerat eros, id gravida arcu. Sed felis purus, tempor ac vestibulum nec, consectetur at purus. 
+                                </p>
+                                
+                                <hr class="mb-30 white"/>
+                                
+                                <h4 class="font-alt">Can I incorporate advertising in my end product with a regular licence?</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula porttitor rutrum. Utore sodales rutrum. Ut eget tristique nibh. Cras mattis est eget lacus dignissim, non consectetur veliterme pellentesque. Ut sit amet placerat eros, id gravida arcu. Sed felis purus, tempor ac vestibulum nec, consectetur at purus. 
+                                </p>
+                                
+                                
+                            </div>
+                        </div>
+                        
+                    </div>
+                </section>
+                <!-- End Section -->
+                
+                
+                <!-- Divider -->
+                <hr class="mt-0 mb-0 white" />
+                <!-- End Divider -->
+                
+                
+                <!-- Call Action Section -->
+                <section class="small-section bg-dark light-content">
+                    <div class="container relative">
+                        <div class="row wow fadeInUpShort">
+                            <div class="col-md-8 offset-md-2 text-center">
+                                <h3 class="call-action-1-heading mb-40 mb-sm-20">Have not find the right answer?</h3>
+                                <div class="local-scroll">
+                                    <a href="pages-contact-1-dark.html" class="btn btn-mod btn-w btn-large btn-round">Contact Us</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- End Call Action Section -->
+                
+                
+            </main>
