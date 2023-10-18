e9348b94e1988d63f30e59770f9f990c76038516
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-18 16:50:25 +0200

    v1.0

--- includes/footer.php
+++ includes/footer.php
@@ -1,48 +1,50 @@
             <!-- Footer -->
             <footer class="page-section bg-dark-lighter light-content pb-100 pb-sm-50">
-                <div class="container-fluid footer-container">
-                    <div class="col-footer-logo">
-                        <div class="nav-logo-wrap local-scroll">
-                            <a href="index.php" class="logo">
-                                <img src="assets/img/logo-white.png" alt="Company logo" width="188" height="37" />
-                            </a>
-                        </div>
-                        <div class="footer-text">
-                            <div class="footer-copy">
-                                <a href="index.html">© Rhythm 2023</a>.
+                <div class="container-fluid">
+                    <div class="row footer-container">
+                        <div class="col-sm-12 col-lg-3 col-footer-logo">
+                            <div class="local-scroll">
+                                <a href="index.php" class="logo">
+                                    <img src="assets/img/logo-white.png" alt="Company logo" width="188" height="37" />
+                                </a>
                             </div>
-                            <div class="footer-made">
-                                Made with love for great people.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-footer-contain">
-                        <div class="row-footer-link">
-                            <div class="promo-footer">
-                                <img src="assets/img/proyecto/app.jpg">
-                            </div>
-                            <!-- 
-                            <div class="col-text">
-                                <p class="uppercase">Respaldando su infraestructura comercial</p>
-                            </div>
-                            -->
-                            <div class="footer-social-links">
-                                <a class="twitter" href="#" title="Twitter" target="_blank"><i class="fab fa-twitter"></i> <span class="sr-only">Twitter profile</span></a>
-                                <a class="instagram" href="#" title="Instagram" target="_blank"><i class="fab fa-instagram"></i> <span class="sr-only">Instagram profile</span></a>
-                                <a class="facebook" href="#" title="Facebook" target="_blank"><i class="fab fa-facebook-f"></i> <span class="sr-only">Facebook profile</span></a>
+                            <div class="footer-text">
+                                <div class="footer-copy">
+                                    <a href="index.html">© Rhythm 2023</a>.
+                                </div>
+                                <div class="footer-made">
+                                    Made with love for great people.
+                                </div>
                             </div>
                         </div>
-                        <hr class="mt-0 mb-0 white" />
-                        <div class="row-footer-btn">
-                            <div class="informacion-legal">
-                                <a href="index.php?route=privacy">Política de privacidad</a>
-                                <a href="index.php?route=cookies">Política de cookies</a>
-                                <a href="index.php?route=aviso">Aviso legal</a></li>
+                        <div class="col-sm-12 col-lg-9 col-footer-contain">
+                            <div class="row-footer-link">
+                                <!-- 
+                                <div class="promo-footer">
+                                    <img src="assets/img/proyecto/app.jpg">
+                                </div>
+                                -->
+                                <div class="col-text">
+                                    <p class="uppercase">Respaldando su infraestructura comercial</p>
+                                </div>
+                                <div class="footer-social-links">
+                                    <a class="twitter" href="#" title="Twitter" target="_blank"><i class="fab fa-twitter"></i> <span class="sr-only">Twitter profile</span></a>
+                                    <a class="instagram" href="#" title="Instagram" target="_blank"><i class="fab fa-instagram"></i> <span class="sr-only">Instagram profile</span></a>
+                                    <a class="facebook" href="#" title="Facebook" target="_blank"><i class="fab fa-facebook-f"></i> <span class="sr-only">Facebook profile</span></a>
+                                </div>
                             </div>
-                            <div>
-                                Calle de Velázquez, 80 Madrid
-                                <br/>
-                                28001 Madrid
+                            <hr class="mt-0 mb-0 white" />
+                            <div class="row-footer-btn">
+                                <div class="row informacion-legal">
+                                    <a class="col-" href="index.php?route=privacy">Política de privacidad</a>
+                                    <a class="col-" href="index.php?route=cookies">Política de cookies</a>
+                                    <a class="col-" href="index.php?route=aviso">Aviso legal</a></li>
+                                </div>
+                                <div>
+                                    Calle de Velázquez, 80 Madrid
+                                    <br/>
+                                    28001 Madrid
+                                </div>
                             </div>
                         </div>
                     </div>
