a52a47b69a6e75dfa360727af8f6b9a861346876
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-18 14:31:55 +0200

    v1.0

--- includes/footer.php
+++ includes/footer.php
@@ -1,41 +1,44 @@
             <!-- Footer -->
             <footer class="page-section bg-dark-lighter light-content pb-100 pb-sm-50">
-                <div class="container">
-                    <div class="footer-info">
-                        <div>
-                            <h5>Sitios</h5>
-                            <ul>
-                                <li><a href="index.php?route=login">Área privada</a></li>
-                                <li><a href="index.php?route=acceso">Plataforma formación</a></li>
-                                <li><a href="index.php?route=app">App documental</a></li>
-                            </ul>
+                <div class="container-fluid footer-container">
+                    <div class="col-footer-logo">
+                        <div class="nav-logo-wrap local-scroll">
+                            <a href="index.php" class="logo">
+                                <img src="assets/img/logo-white.png" alt="Company logo" width="188" height="37" />
+                            </a>
                         </div>
-
-                        <div>
-                            <h5>Atención al cliente</h5>
-                            Calle de Velázquez, 80 Madrid
-                            <br/>
-                            28001 Madrid
-                            <br/>
-                            Teléfono: 912354364
-                            <br/>
-                            Correo: area14@gmail.com
+                        <div class="footer-text">
+                            <div class="footer-copy">
+                                <a href="index.html">© Rhythm 2023</a>.
+                            </div>
+                            <div class="footer-made">
+                                Made with love for great people.
+                            </div>
                         </div>
-
-                        <!-- Social Links -->
-                        <div class="footer-social-links">
-                            <a href="#" title="Facebook" target="_blank"><i class="fab fa-facebook-f"></i> <span class="sr-only">Facebook profile</span></a>
-                            <a href="#" title="Twitter" target="_blank"><i class="fab fa-twitter"></i> <span class="sr-only">Twitter profile</span></a>
-                            <a href="#" title="Instagram" target="_blank"><i class="fab fa-instagram"></i> <span class="sr-only">Instagram profile</span></a>
-                        </div>
-                        <!-- End Social Links -->
                     </div>
-
-                    <div class="informacion-legal">
-                        <div class="content-informacion-legal">
-                            <a class="active" href="index.php?route=privacy">Política de privacidad</a>
-                            <a href="index.php?route=cookies">Política de cookies</a>
-                            <a href="index.php?route=aviso">Aviso legal</a></li>
+                    <div class="col-footer-contain">
+                        <div class="row-footer-link">
+                            <div class="col-text">
+                                <p class="uppercase">Respaldando su infraestructura comercial</p>
+                            </div>
+                            <div class="footer-social-links">
+                                <a class="twitter" href="#" title="Twitter" target="_blank"><i class="fab fa-twitter"></i> <span class="sr-only">Twitter profile</span></a>
+                                <a class="instagram" href="#" title="Instagram" target="_blank"><i class="fab fa-instagram"></i> <span class="sr-only">Instagram profile</span></a>
+                                <a class="facebook" href="#" title="Facebook" target="_blank"><i class="fab fa-facebook-f"></i> <span class="sr-only">Facebook profile</span></a>
+                            </div>
+                        </div>
+                        <hr class="mt-0 mb-0 white" />
+                        <div class="row-footer-btn">
+                            <div class="informacion-legal">
+                                <a href="index.php?route=privacy">Política de privacidad</a>
+                                <a href="index.php?route=cookies">Política de cookies</a>
+                                <a href="index.php?route=aviso">Aviso legal</a></li>
+                            </div>
+                            <div>
+                                Calle de Velázquez, 80 Madrid
+                                <br/>
+                                28001 Madrid
+                            </div>
                         </div>
                     </div>
                 </div>
