b7aef528462fd90e7750f107d03810c1276074ed
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-10-19 09:56:02 +0200

    v1.0

--- includes/style.php
+++ includes/style.php
@@ -1,7 +1,7 @@
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/style.css">
-        <!--<link rel="stylesheet" href="assets/css/style-responsive.css">
-        <link rel="stylesheet" href="assets/css/vertical-rhythm.min.css">-->
+        <!--<link rel="stylesheet" href="assets/css/style-responsive.css">-->
+        <link rel="stylesheet" href="assets/css/vertical-rhythm.min.css">
         <link rel="stylesheet" href="assets/css/magnific-popup.css">
         <link rel="stylesheet" href="assets/css/owl.carousel.css">
         <link rel="stylesheet" href="assets/css/animate.min.css">
